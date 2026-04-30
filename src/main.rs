fd175164ff764310842cac00ef74bcd2a3167814
tkahng <tkahng@gmail.com>
2026-04-30 14:51:33 -0700

    feat: make lexer iterable

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,36 @@ impl<'a> Lexer<'a> {
     fn new(content: &'a [char]) -> Self {
         Self { content }
     }
+
+    fn trim_left(&mut self) {
+        while self.content.len() > 0 && self.content[0].is_whitespace() {
+            self.content = &self.content[1..]
+        }
+    }
+
+    fn next_token(&mut self) -> Option<&'a [char]> {
+        self.trim_left();
+        if self.content.len() == 0 {
+            return None;
+        }
+        if self.content[0].is_alphabetic() {
+            let mut n = 0;
+            while n < self.content.len() && self.content[0].is_alphanumeric() {
+                n += 1;
+            }
+            let token = &self.content[0..n];
+            self.content = &self.content[n..];
+            return Some(token);
+        }
+        todo!()
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = &'a [char];
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next_token()
+    }
 }
 
 fn index(content: &str) -> HashMap<String, usize> {
@@ -28,6 +58,7 @@ fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
     for event in eventReader.into_iter() {
         if let XmlEvent::Characters(text) = event.expect("TODO: ") {
             content.push_str(&text);
+            content.push_str(" ");
         }
     }
     Ok(content)
@@ -37,8 +68,9 @@ fn main() -> io::Result<()> {
         .chars()
         .collect::<Vec<_>>();
 
-    let lexer = Lexer::new(&content);
-    println!("{lexer:?}");
+    for token in Lexer::new(&content) {
+        println!("{token:?}");
+    }
     // let all_documents = HashMap::<Path, HashMap<String, usize>>::new();
     // let dir_path = "docs.gl/gl4/";
     // let dir = fs::read_dir(dir_path)?;
